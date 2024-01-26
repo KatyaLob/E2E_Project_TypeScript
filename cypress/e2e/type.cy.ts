c3b6a8f2afc38d52f7e1949c263cb50011304017
Katya Lob <klobynicheva@hotmail.com>
2024-01-26 14:18:01 -0600

    fixed type spec

--- cypress/e2e/type.cy.ts
+++ cypress/e2e/type.cy.ts
@@ -4,11 +4,11 @@ describe('Text Input', () => {
   it('text test', () => {
     cy.visit('/textinput')
     cy.get('[id="newButtonName"]').type(textName)
-    cy.contains('button','Button that Should').click()
+    cy.contains('button','Button That Should').click()
     cy.get('.btn-primary').then(element=>{
       console.log(element.text(),'text')
       cy.log(element.text())
-      cy.wrap(element.text()).should('have.text',textName)
+      cy.wrap(element).should('have.text',textName)
     })
     cy.get('.form-group').within(text=>{
       console.log(text.text(),'text')
