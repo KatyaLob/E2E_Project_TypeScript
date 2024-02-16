9fda3f32004b2bca65e05c72b40e00de05cf85c2
Katya Lob <klobynicheva@hotmail.com>
2024-02-15 23:47:57 -0600

    reporter

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,29 +1,41 @@
-import { defineConfig } from 'cypress';
+import {defineConfig} from 'cypress';
+
 require('dotenv').config()
 export default defineConfig({
-  e2e: {
-    baseUrl: 'https://uitestingplayground.com',
-    //baseUrl: 'https://play1.automationcamp.ir',
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    reporter: 'cypress-mochawesome-reporter',
+    e2e: {
+        baseUrl: 'https://uitestingplayground.com',
+        //baseUrl: 'https://play1.automationcamp.ir',
+        setupNodeEvents(on, config) {
+            //reporter
+            require('cypress-mochawesome-reporter/plugin')(on);
+            // implement node event listeners here
+        },
+        env: {
+            stage: 'https://stage.pasv.us/user/login',
+            prod: 'https://coding.pasv.us',
+            test: 'Hello World!',
+            demoQA: 'https://demoqa.com',
+            expected_conditions: 'https://play1.automationcamp.ir/expected_conditions.html',
+            email: 'klobynicheva@hotmail.com',
+            password: 'HelloWorld1',
+            herokuapp: 'https://the-internet.herokuapp.com'
+        },
     },
-    env: {
-      stage: 'https://stage.pasv.us/user/login',
-      prod: 'https://coding.pasv.us',
-      test: 'Hello World!',
-      demoQA:'https://demoqa.com',
-      expected_conditions: 'https://play1.automationcamp.ir/expected_conditions.html',
-      email: 'klobynicheva@hotmail.com',
-      password: 'HelloWorld1',
-      herokuapp: 'https://the-internet.herokuapp.com'
+    viewportWidth: 1200,
+    viewportHeight: 1400,
+    retries: {
+        runMode: 2,
+        openMode: 1,
     },
-  },
-  viewportWidth: 1200,
-  viewportHeight: 1400,
-  retries: {
-    runMode: 2,
-    openMode: 1,
-  },
-
-  defaultCommandTimeout: 16000
+    video: true,
+    screenshotOnRunFailure: true,
+    defaultCommandTimeout: 16000,
+    reporterOptions: {
+        charts: true,
+        reportPageTitle: 'UIClasses',
+        embeddedScreenshots: true,
+        inlineAssets: true,
+        saveAllAttempts: false
+    },
 });
