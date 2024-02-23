61733de1952543b1f5a5493d6112dfb54371957c
Katya Lob <klobynicheva@hotmail.com>
2024-02-23 00:17:22 -0600

    excel

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,5 +1,9 @@
 import {defineConfig} from 'cypress';
 
+const xlsx = require("node-xlsx").default;
+const fs = require("fs"); // for file
+const path = require("path")
+
 require('dotenv').config()
 export default defineConfig({
     reporter: 'cypress-mochawesome-reporter',
@@ -10,6 +14,19 @@ export default defineConfig({
             //reporter
             require('cypress-mochawesome-reporter/plugin')(on);
             // implement node event listeners here
+            //reading excel document from fixture
+            on("task", {
+                parseXlsx({ filePath }) {
+                    return new Promise((resolve, reject) => {
+                        try {
+                            const jsonData = xlsx.parse(fs.readFileSync(filePath));
+                            resolve(jsonData);
+                        } catch (e) {
+                            reject(e);
+                        }
+                    });
+                },
+            });
         },
         env: {
             stage: 'https://stage.pasv.us',
